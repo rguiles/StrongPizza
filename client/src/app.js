8267d95d09467109c13f5c4d451d6bd3e5381fd3
rguiles <rguiles@csub.edu>
2022-11-15 21:45:19 -0700

    basic css edits

--- client/src/app.js
+++ client/src/app.js
@@ -2,12 +2,20 @@ import React from "react";
 import logo from "./pizza.png";
 import "./app.css";
 
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import createTopping from "./components/createTopping";
+// import toppingList from "./components/toppingList";
+
+// const routes = require('../../server/routes/api/toppings')
+// App.use('/api', routes);
+
 // const express = require("express");
 // const App = express();
 // const connectDB = require(".//config/db");
 // connectDB();
 
 function App() {
+  
   const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
@@ -19,19 +27,40 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Strong Pizza Management</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
         <a
           className="App-link"
           href="https://strongpizza.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Make Pizza
+          Topping Inventory
+        </a>
+        <a
+          className="App-link"
+          href="https://strongpizza.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Make Pizza
         </a>
       </header>
     </div>
   );
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <Route exact path='/' component={toppingList} />
+//           <Route path='/create-topping' component={createTopping} />
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
 
 export default App;
